3e86d4fe28b3108c3d545561e66ca82623c318a1
Walid Ara <arawalid90@gmail.com>
2024-05-10 12:28:51 +0100

    first init

---
+++ react/src/hooks/useFetch.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosRequestConfig } from "axios"; // Install axios package by following this command npm install axios
+
+type Fetch = {
+  endPoint: string; // replace this one with all possible endpoints of better response
+  method: string;
+  body?: any;
+  TokenInclude?: boolean;
+  token?: string;
+};
+
+export const useFetch = ({
+  endPoint,
+  method,
+  TokenInclude,
+  body,
+  token,
+}: Fetch) => {
+  try {
+    const header = process.env.TOKEN_HEADER; // if you are using Next JS replace with NEXT_PUBLIC_TOKEN_HEADER
+    const fetchData = async () => {
+      const axiosConfig: AxiosRequestConfig = {
+        method: method.toUpperCase(),
+        url: endPoint,
+        headers: {
+          "Content-Type": "application/json",
+          ...(TokenInclude && { [header]: token }),
+        },
+        data: body,
+      };
+
+      const response = await axios(axiosConfig);
+      return response.data;
+    };
+
+    return fetchData();
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
